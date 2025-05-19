a4bd5f071361337089931f676475734b83ca33e6
Frank Hurtado <frank.montenegro@correounivalle.edu.co>
2025-05-19 11:16:47 -0500

    Stop tracking postgresql/data and add to .gitignore

---
+++ cedenar_anomalies/application/load_to_anomalia_db.py
@@ -0,0 +1,49 @@
+# cedenar_anomalies/application/load_to_anomalia_db.py
+import argparse
+import logging
+from pathlib import Path
+
+from cedenar_anomalies.domain.services.anomalia_service import AnomaliaService
+from cedenar_anomalies.infrastructure.adapters.repositories.sql_anomalia_repository import (
+    SQLAnomaliaRepository,
+)
+from cedenar_anomalies.utils.config import PROCESSED_DATA_DIR
+
+# Configurar logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Carga datos de anomalías a la base de datos PostgreSQL"
+    )
+    parser.add_argument(
+        "--file",
+        type=str,
+        help="Ruta al archivo CSV",
+        default=str(PROCESSED_DATA_DIR / "dataset_limpio_2023.csv"),
+    )
+    parser.add_argument("--year", type=int, help="Año de los datos", default=2023)
+
+    args = parser.parse_args()
+
+    file_path = Path(args.file)
+    if not file_path.exists():
+        logger.error(f"El archivo {file_path} no existe")
+        return
+
+    # Inicializar componentes
+    repository = SQLAnomaliaRepository(logger=logger)
+    service = AnomaliaService(repository, logger=logger)
+
+    # Ejecutar caso de uso
+    saved_count = service.load_anomalias_from_csv(str(file_path), args.year)
+
+    logger.info(f"Proceso completado: se guardaron {saved_count} registros")
+
+
+if __name__ == "__main__":
+    main()
--- cedenar_anomalies/infrastructure/database/models.py
+++ cedenar_anomalies/infrastructure/database/models.py
@@ -48,12 +48,44 @@ class AnomaliaData(Base):
     # Metadata
     fecha_creacion = Column(String(50), nullable=True)  # Fecha de creación del registro
 
-    # Para búsqueda por texto
-    # tags = Column(MutableList.as_mutable(ARRAY(String)), nullable=True)
 
-    def __repr__(self):
-        return (
-            f"<AnomaliaData(id={self.id},",
-            f" odt={self.odt},",
-            f" Anomalia_conf={self.Anomalia_conf})>",
-        )
+
+class InferenceModel(Base):
+    __tablename__ = "inference_model"
+
+    # Definir un ID autoincremental como clave primaria
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    # Campos basados en el DataFrame proporcionado
+    Orden = Column(Integer, nullable=False)
+    Usuario = Column(Integer, nullable=False)
+    Ejecucion = Column(String(255), nullable=False)
+    Codigo = Column(String(255), nullable=True)
+    Descripcion = Column(Text, nullable=False)
+    Motivo = Column(Text, nullable=True)
+    kWh_Rec = Column(
+        Float, nullable=True
+    )  # Se cambió 'kWh Rec' a 'kWh_Rec' para evitar espacios
+    Factor = Column(Float, nullable=True)
+    data_id = Column(Float, nullable=True)  # Renombrado desde 'id' para evitar conflicto
+    Nombre = Column(String(255), nullable=True)
+    Factor_1 = Column(
+        Float, nullable=True
+    )  # Se cambió 'Factor.1' a 'Factor_1' para evitar puntos
+    AREA = Column(String(255), nullable=True)
+    PLAN_COMERCIAL = Column(String(255), nullable=True)
+    TRAFO_OPEN = Column(String(255), nullable=True)
+    FASES = Column(Float, nullable=True)
+    KVA = Column(Float, nullable=True)
+    LATI_USU = Column(Float, nullable=True)
+    LONG_USU = Column(Float, nullable=True)
+    puntaje = Column(Float, nullable=True)
+    evaluacion = Column(Float, nullable=True)
+    Zona = Column(String(255), nullable=False)
+    cluster_id = Column(Integer, nullable=False)
+    cluster_0 = Column(Float, nullable=False)
+    cluster_1 = Column(Float, nullable=False)
+    cluster_2 = Column(Float, nullable=False)
+
+    # Metadata
+    fecha_carga = Column(String(50), nullable=True)  # Fecha de carga del registro
